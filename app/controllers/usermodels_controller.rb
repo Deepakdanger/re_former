4ca8ff4c838eaedc04ee84e724af3497bf2143e8
Deepakdanger <depk.96.kumar@gmail.com>
2021-05-27 15:27:13 +0530

    Add the input field

--- app/controllers/usermodels_controller.rb
+++ app/controllers/usermodels_controller.rb
@@ -1,8 +1,14 @@
 class UsermodelsController < ApplicationController
     def new
-        @usermodel = Usermodel.new
     end
 
     def create
+        @user = Usermodel.new(username: params[:username], email: params[:email], password: params[:password])
+
+        if @user.save
+          redirect_to  new_usermodel_path
+        else
+          render :new
+        end
     end
 end
