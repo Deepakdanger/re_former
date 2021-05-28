80c194a9956a9bcc33a50c73e52d424e09404a02
Deepakdanger <depk.96.kumar@gmail.com>
2021-05-28 15:29:22 +0530

    Done the form_tag

--- app/controllers/usermodels_controller.rb
+++ app/controllers/usermodels_controller.rb
@@ -1,20 +1,21 @@
 class UsermodelsController < ApplicationController
     def new
+      @user = Usermodel.new  
     end
 
     def create
-        @user = Usermodel.new(user_params)
+        @user = Usermodel.new(usermodel_params)
       
         if @user.save
-          redirect_to new_usermodels_path
+          redirect_to new_usermodel_path
         else
           render :new
         end
-      end
+    end
 
-  private
+    private
 
-  def user_params
-    params.require(:usermodels).permit(:username, :email, :password)
-  end
+    def usermodel_params
+      params.require(:usermodel).permit(:username, :email, :password)
+    end
 end
