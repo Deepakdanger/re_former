b7655c94133530dea0da1121b00779baf9b8e097
Deepakdanger <depk.96.kumar@gmail.com>
2021-06-01 12:08:56 +0530

    Add routes in index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@
 
 Rails.application.routes.draw do
   root "usermodels#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :usermodels, only: %i[new create edit update show]
+  resources :usermodels, only: %i[new create edit update]
 end
